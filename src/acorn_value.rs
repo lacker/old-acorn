318be9068c6bb0aba1ba947109d51ffae46d0c61
Kevin Lacker <lacker@gmail.com>
2023-03-02 16:50:42 -0800

    flip stack indexing

--- src/acorn_value.rs
+++ src/acorn_value.rs
@@ -27,15 +27,8 @@ pub enum Atom {
     Skolem(usize),
 
     // A Reference is a reference to a variable on the stack.
-    // We drop the variable name. Instead we track the "depth" of the binding.
-    // Although we store arg lists as a vector, we consider the leftmost parts to be "higher up" in
-    // the syntax tree.
-    // So in the expression:
-    // define foo(x: bool, y: bool) -> bool = bar(x, y)
-    // In bar(x, y), the binding for x is at depth 1, and the binding for y is at depth 0.
-    // We do this counting-backwards thing so that we can clone an AcornValue to use it in other
-    // values without having to account for different stacks.
-    // (This might be premature optimization.)
+    // We drop the variable name. Instead we track the index on the stack of the binding.
+    // This does mean that you must be careful when moving values between different stack environments.
     Reference(usize),
 }
 
