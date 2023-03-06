fd3c3f5096ccffedf2cd3f4a4fbf89452999d3f7
Kevin Lacker <lacker@gmail.com>
2023-03-06 11:23:36 -0800

    handle non prenex

--- src/skolemizer.rs
+++ src/skolemizer.rs
@@ -14,6 +14,21 @@ impl Skolemizer {
 
     // The input should already have negations moved inwards.
     // The stack must be entirely universal quantifiers.
+    //
+    // The value does *not* need to be in prenex normal form.
+    // I.e., it can still have quantifier nodes, either "exists" or "forall", inside of
+    // logical nodes, like "and" and "or".
+    // All negations must be moved inside quantifiers, though.
+    //
+    // In general I think converting to prenex seems bad. Consider:
+    //   forall(x, f(x)) & exists(y, g(y))
+    // If we convert to prenex, we get:
+    //   forall(x, exists(y, f(x) & g(y)))
+    // which skolemizes to
+    //   forall(x, f(x) & g(skolem(x)))
+    // But there's a redundant arg here. The simpler form is just
+    //   forall(x, f(x) & g(skolem()))
+    // which is what we get if we don't convert to prenex first.
     pub fn skolemize(&mut self, stack: &Vec<AcornType>, value: AcornValue) -> AcornValue {
         match value {
             AcornValue::ForAll(quants, subvalue) => {
@@ -21,6 +36,7 @@ impl Skolemizer {
                 new_stack.extend(quants);
                 self.skolemize(&new_stack, *subvalue)
             }
+
             AcornValue::Exists(quants, subvalue) => {
                 // The current stack will be the arguments for the skolem functions
                 let mut args = vec![];
@@ -56,9 +72,29 @@ impl Skolemizer {
                 self.skolemize(stack, subvalue.bind_values(stack.len(), &replacements))
             }
 
-            // Skolemization only affects top-level chains of "forall" and "exists" nodes.
-            // We could do some validation here, but we don't.
-            _ => value,
+            AcornValue::And(left, right) => {
+                let left = self.skolemize(stack, *left);
+                let right = self.skolemize(stack, *right);
+                AcornValue::And(Box::new(left), Box::new(right))
+            }
+
+            AcornValue::Or(left, right) => {
+                let left = self.skolemize(stack, *left);
+                let right = self.skolemize(stack, *right);
+                AcornValue::Or(Box::new(left), Box::new(right))
+            }
+
+            // Acceptable terminal nodes for the skolemization algorithm
+            AcornValue::Atom(_) => value,
+            AcornValue::Application(_) => value,
+            AcornValue::Not(_) => value,
+            AcornValue::Equals(_, _) => value,
+            AcornValue::NotEquals(_, _) => value,
+
+            _ => panic!(
+                "moving negation inwards should have eliminated this node: {:?}",
+                value
+            ),
         }
     }
 }
