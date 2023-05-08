428731d394e60b1b05193e85db3f913b81da6a2b
Kevin Lacker <lacker@gmail.com>
2023-05-08 11:15:13 -0700

    making term graph stuff

--- src/term_graph.rs
+++ src/term_graph.rs
@@ -1,1 +1,60 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::atom::{Atom, AtomId};
+use crate::type_space::TypeId;
+
+// The TermInfo stores information about an abstract term.
+// The idea behind the abstract term is that if we know two terms are identical, like:
+//   double(x)
+//   add(x, x)
+// then we can store them as the same abstract term. Instead of rewriting terms, we can
+// merge nodes in our graph of abstract terms. If we are careful about always merging the
+// "smaller" node into the "larger" one, we can do these merges cheaply (amortized).
+pub struct TermInfo {
+    term_type: TypeId,
+    arg_types: Vec<TypeId>,
+
+    // All edges that touch this term
+    adjacent: HashSet<EdgeId>,
+
+    // The canonical way to produce this term.
+    canonical: CanonicalForm,
+}
 pub type TermId = u32;
+
+// An edge represents a single substitution.
+// All variables get renamed, and some in the template can get replaced.
+// For example:
+// template = add(x0, x1)
+// replacement = mul(x0, x1)
+//
+pub struct EdgeInfo {
+    // The base term that will be substituted into
+    template: TermId,
+
+    // variable i in template gets mapped to var_map[i] after substitution.
+    // "None" means "replace with the replacement"
+    template_var_map: Vec<Option<AtomId>>,
+
+    // variable i in replacement gets mapped to variable varmap[i]
+    replacement: TermId,
+    replacement_var_map: Vec<AtomId>,
+
+    // What we get
+    result: TermId,
+}
+pub type EdgeId = u32;
+
+// The canonical form of a term can either be an atom, or it can be a particular way
+// of constructing this term.
+enum CanonicalForm {
+    Atom(Atom),
+    Edge(EdgeId),
+}
+
+pub struct TermGraph {
+    terms: Vec<TermInfo>,
+    edges: Vec<EdgeInfo>,
+
+    edgemap: HashMap<(TermId, TermId), Vec<EdgeId>>,
+}
