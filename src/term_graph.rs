10a57d64220711dbfa04e6b62b196c636b95faed
Kevin Lacker <lacker@gmail.com>
2023-05-09 09:45:32 -0700

    checking edge keys

--- src/term_graph.rs
+++ src/term_graph.rs
@@ -28,12 +28,13 @@ pub type TermId = u32;
 
 // TermInfo normalizes across all namings of the input variables.
 // A TermInstance represents a particular ordering.
-// varmap[i] = j means that TermInstance replaces x_i with x_j.
+// var_map[i] = j means that the variable the TermInfo would display as x_i, the TermInstance would
+// display as x_j.
 // So for example to represent a TermInstance:
 //   foo(x4, x0)
 // we would use:
 //   term: foo(x0, x1)
-//   varmap: [4, 0]
+//   var_map: [4, 0]
 // The length of varmap must be the same as arg_types.
 // No two variables should be named the same thing.
 pub struct TermInstance {
@@ -41,107 +42,93 @@ pub struct TermInstance {
     var_map: Vec<AtomId>,
 }
 
+impl TermInstance {
+    // Panics if there is a logical inconsistency
+    pub fn check(&self) {
+        let mut reverse_map = vec![None; self.var_map.len()];
+        for (i, &var) in self.var_map.iter().enumerate() {
+            if let Some(old) = reverse_map[var as usize] {
+                panic!(
+                    "TermInstance has two variables named {} (at {} and {})",
+                    var, i, old
+                );
+            }
+            reverse_map[var as usize] = Some(i as AtomId);
+        }
+    }
+}
+
+// The different ways to replace a single variable in a substitution.
+// Either we rename the variable, or we expand it into a different term.
+// "Do-nothing" replacements are represented by a Rename with the same index.
+pub enum Replacement {
+    Rename(AtomId),
+    Expand(TermInstance),
+}
+
+impl Replacement {
+    // Calls f on each variable id used by this replacement.
+    fn map_vars(&self, f: &mut impl FnMut(&AtomId)) {
+        match self {
+            Replacement::Rename(var) => f(var),
+            Replacement::Expand(term) => {
+                for &var in &term.var_map {
+                    f(&var);
+                }
+            }
+        }
+    }
+}
+
 // An edge represents a single substitution.
+// An EdgeKey is enough information to uniquely specify one substitution.
 // All variables get renamed, and some in the template can get replaced.
 // For example:
 // template = add(x0, x1)
 // replacement = mul(x0, x1)
-pub struct EdgeInfo {
+pub struct EdgeKey {
     // The base term that will be substituted into
     template: TermId,
 
-    // variable i in template gets renamed to variable var_map[i] after substitution.
-    // "None" means "replace with the replacement"
-    template_var_map: Vec<Option<AtomId>>,
+    // The replacement for each variable in the template.
+    // Should have as many entries as the template has variables.
+    // This list is normalized so that a single substitution is represented in only one way.
+    // The first time a new variable appears, it should get the first available index.
+    replacements: Vec<Replacement>,
+}
 
-    // variable i in replacement gets renamed to variable varmap[i]
-    replacement: TermId,
-    replacement_var_map: Vec<AtomId>,
+impl EdgeKey {
+    // Panics if this edge is not normalized.
+    pub fn check(&self) {
+        // We expect to see the variables in increasing order
+        let mut expected = 0;
 
-    // What we get.
-    // The number of variables in the result can be different, even for edges that share the same
-    // template and replacement, when the var maps are different.
-    result: TermId,
-    result_num_vars: AtomId,
+        for replacement in &self.replacements {
+            replacement.map_vars(&mut |&var| {
+                if var > expected {
+                    panic!(
+                        "EdgeKey is not normalized: expected {}, got {}",
+                        expected, var
+                    );
+                }
+                if var == expected {
+                    expected += 1;
+                }
+            });
+        }
+    }
+}
+
+pub struct EdgeInfo {
+    // The parameters that determine the substitution
+    key: EdgeKey,
+
+    // The result of the substitution
+    result: TermInstance,
 }
 pub type EdgeId = u32;
 
-impl EdgeInfo {
-    // Check whether an edge matches two instances.
-    // This fails if the instances are using term indexing in a way that doesn't match this edge.
-    // If it succeeds, it returns a map of (var index -> instance index) usable to construct a
-    // TermInstance we get by following this edge.
-    // position is relative to the instance.
-    fn matches(
-        &self,
-        template_instance: &TermInstance,
-        position: AtomId,
-        replacement_instance: &TermInstance,
-    ) -> Option<Vec<AtomId>> {
-        // Leave a "None" in any entry when we don't know what it is yet
-        let mut output_var_map: Vec<Option<AtomId>> = vec![None; self.result_num_vars as usize];
-
-        // Check which variables in the output come from the template
-        assert_eq!(self.template, template_instance.term);
-        assert_eq!(self.template_var_map.len(), template_instance.var_map.len());
-        for (i, instance_i) in template_instance.var_map.iter().enumerate() {
-            // Consider variable i in the underlying template, which is called instance_i
-            // in this instance.
-            if let Some(template_var) = self.template_var_map[i] {
-                // The edge renames it to template_var in the output.
-                // Thus our output instance should have output_var_map[instance_i] = template_var.
-                if let Some(output_var) = output_var_map[*instance_i as usize] {
-                    // If we already have a value for this variable, it should match.
-                    if output_var != template_var {
-                        return None;
-                    }
-                } else {
-                    // Otherwise, we should set it.
-                    output_var_map[*instance_i as usize] = Some(template_var);
-                }
-            } else {
-                // The edge thinks that variable i should be replaced.
-                // Thus instance_i should be position.
-                if *instance_i != position {
-                    return None;
-                }
-            }
-        }
-
-        // Check which variables in the output come from the replacement
-        assert_eq!(self.replacement, replacement_instance.term);
-        assert_eq!(
-            self.replacement_var_map.len(),
-            replacement_instance.var_map.len()
-        );
-        for (i, instance_i) in replacement_instance.var_map.iter().enumerate() {
-            // Consider variable i n the underlying replacement, which is called instance_i
-            // in this instance.
-            // The edge renames it to replacement_var_map[i] in the output.
-            let replacement_var = self.replacement_var_map[i];
-            if let Some(output_var) = output_var_map[*instance_i as usize] {
-                // If we already have a value for this variable, it should match.
-                if output_var != replacement_var {
-                    return None;
-                }
-            } else {
-                // Otherwise, we should set it.
-                output_var_map[*instance_i as usize] = Some(replacement_var);
-            }
-        }
-
-        // To be a valid output map, every variable should be set.
-        let mut answer = vec![];
-        for (i, output_var) in output_var_map.iter().enumerate() {
-            if let Some(output_var) = output_var {
-                answer.push(*output_var);
-            } else {
-                return None;
-            }
-        }
-        Some(answer)
-    }
-}
+impl EdgeInfo {}
 
 // The canonical form of a term can either be an atom, or it can be a particular way
 // of constructing this term.
@@ -156,7 +143,7 @@ pub struct TermGraph {
     edges: Vec<EdgeInfo>,
 
     // Maps (template, replacement) -> edges
-    edgemap: FxHashMap<(TermId, TermId), Vec<EdgeId>>,
+    edgemap: FxHashMap<EdgeKey, EdgeId>,
 }
 
 impl TermGraph {
@@ -171,16 +158,6 @@ impl TermGraph {
 
     pub fn get_term_info(&self, term: TermId) -> &TermInfo {
         &self.terms[term as usize]
-    }
-
-    // An instance with the variables numbered in standard order.
-    pub fn instance(&self, term: TermId) -> TermInstance {
-        let num_vars = self.get_term_info(term).arg_types.len();
-        let varmap: Vec<AtomId> = (0..num_vars).map(|i| i as AtomId).collect();
-        TermInstance {
-            term,
-            var_map: varmap,
-        }
     }
 
     // Inserts a term for an atom, or returns the existing term if there is one.
@@ -208,21 +185,6 @@ impl TermGraph {
         position: AtomId,
         replacement: &TermInstance,
     ) -> TermInstance {
-        // Check if any existing edge covers this case
-        let key = (template.term, replacement.term);
-        if let Some(edges) = self.edgemap.get(&key) {
-            for edge_id in edges {
-                let edge = &self.edges[*edge_id as usize];
-                if let Some(var_map) = edge.matches(template, position, replacement) {
-                    return TermInstance {
-                        term: edge.result,
-                        var_map,
-                    };
-                }
-            }
-        }
-
-        // We need to create a new term.
         todo!("replace");
     }
 
