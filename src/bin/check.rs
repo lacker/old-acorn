0d08ad24ee7ba618024e7d37374f518a05c8652b
Kevin Lacker <lacker@gmail.com>
2023-07-19 14:23:46 -0700

    final step of the block works

--- src/bin/check.rs
+++ src/bin/check.rs
@@ -8,39 +8,27 @@ use acorn::prover::{Outcome, Prover};
 
 const USAGE: &str = "Usage: cargo run --bin=check <filename>";
 
-// Proves a theorem, without doing any recursion.
-// It is assumed that all claims we need to prove this theorem are provided in claims.
-fn prove_one(env: &Environment, claims: &Vec<AcornValue>, theorem: &Proposition) {
-    let theorem_string = if let Some(name) = &theorem.display_name {
-        name.to_string()
-    } else {
-        env.value_str(&theorem.claim)
-    };
-
-    if theorem.proven {
-        // Don't need to prove these
-        println!("{} is axiomatic", theorem_string);
-        return;
-    }
-
+// Proves one proposition, without doing any recursion.
+// It is assumed that all facts we need to prove this theorem are provided in facts.
+fn prove_one(env: &Environment, facts: &Vec<AcornValue>, goal_name: &str, goal_value: &AcornValue) {
     let mut prover = Prover::new(&env);
     prover.verbose = false;
 
-    for claim in claims {
+    for claim in facts {
         prover.add_proposition(claim.clone());
     }
-    prover.add_negated(env.expand_constants(&theorem.claim));
+    prover.add_negated(env.expand_constants(&goal_value));
 
     let outcome = prover.search_for_contradiction(1000, 1.0);
     match outcome {
         Outcome::Success => {
-            println!("{} proved", theorem_string);
+            println!("{} proved", goal_name);
         }
         Outcome::Failure => {
-            println!("{} is unprovable", theorem_string);
+            println!("{} is unprovable", goal_name);
         }
         Outcome::Unknown => {
-            println!("{} could not be proved", theorem_string);
+            println!("{} could not be proved", goal_name);
         }
     }
 }
@@ -49,25 +37,32 @@ fn prove_one(env: &Environment, claims: &Vec<AcornValue>, theorem: &Proposition)
 // If the theorem is axiomatic, it is just assumed to be true.
 // If there are claims within the body, those are proved first, and used to prove the theorem's
 // main claim.
-// After the proof, 'claims' is updated to contain the new claim.
-fn prove_rec(env: &Environment, claims: &mut Vec<AcornValue>, theorem: &Proposition) {
-    if let Some(block) = &theorem.block {
-        let claims_len = claims.len();
+// After the proof, 'facts' is updated to contain the new proposition.
+fn prove_rec(env: &Environment, facts: &mut Vec<AcornValue>, goal: &Proposition) {
+    let goal_name = if let Some(name) = &goal.display_name {
+        name.to_string()
+    } else {
+        env.value_str(&goal.claim)
+    };
+    if let Some(block) = &goal.block {
+        let facts_len = facts.len();
 
         // Prove all the claims within the block
-        for theorem in &block.env.propositions {
-            prove_rec(&block.env, claims, theorem);
+        for prop in &block.env.propositions {
+            prove_rec(&block.env, facts, prop);
         }
 
-        // Prove the main claim
-        prove_one(&block.env, claims, theorem);
+        // Prove the block's main goal
+        prove_one(&block.env, facts, &goal_name, &block.claim);
 
         // Drop the subclaims
-        claims.truncate(claims_len);
+        facts.truncate(facts_len);
+    } else if goal.proven {
+        println!("{} is axiomatic", goal_name);
     } else {
-        prove_one(env, claims, theorem);
+        prove_one(env, facts, &goal_name, &goal.claim);
     }
-    claims.push(env.expand_constants(&theorem.claim));
+    facts.push(env.expand_constants(&goal.claim));
 }
 
 fn main() {
@@ -79,9 +74,9 @@ fn main() {
     let mut env = Environment::new();
     env.load_file(&input_file).unwrap();
 
-    // Once each theorem gets proved, we add its claim
-    let mut claims: Vec<AcornValue> = Vec::new();
+    // Once each theorem gets proved, we add it to the fact list
+    let mut facts: Vec<AcornValue> = Vec::new();
     for theorem in &env.propositions {
-        prove_rec(&env, &mut claims, theorem);
+        prove_rec(&env, &mut facts, theorem);
     }
 }
