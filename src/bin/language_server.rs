8bc34e49e9a4726955980be35f6baa5f69f1033a
Kevin Lacker <lacker@gmail.com>
2023-09-21 12:07:26 -0700

    lsp server that just logs when file saved

--- src/bin/language_server.rs
+++ src/bin/language_server.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world! This is the language server.");
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::{
+    DidSaveTextDocumentParams, InitializeParams, InitializeResult, MessageType, ServerCapabilities,
+};
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            server_info: None,
+            capabilities: ServerCapabilities::default(),
+        })
+    }
+
+    async fn did_save(&self, _: DidSaveTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file saved!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::build(|client| Backend { client }).finish();
+
+    Server::new(stdin, stdout, socket).serve(service).await;
 }
