f16d6e7f9fe3e1e1cc55becfb59b0ab0bf717fd0
Kevin Lacker <lacker@gmail.com>
2023-09-25 10:47:12 -0700

    compiles

--- src/bin/language_server.rs
+++ src/bin/language_server.rs
@@ -1,4 +1,5 @@
 use acorn::environment::Environment;
+use acorn::token::Error;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
@@ -27,7 +28,39 @@ impl Backend {
             }
             Err(e) => {
                 self.log_info(&format!("env.add failed: {:?}", e)).await;
-                // TODO: send diagnostic
+
+                let range = match &e {
+                    Error::Token(token_error) => {
+                        let line = token_error.line_index as u32;
+                        let character = token_error.char_index as u32;
+                        let end_character = character + token_error.text.len() as u32;
+                        let start = Position { line, character };
+                        let end = Position {
+                            line,
+                            character: end_character,
+                        };
+                        Range { start, end }
+                    }
+                    Error::EOF => {
+                        let line = doc.text.lines().count() as u32;
+                        let character = doc.text.lines().last().unwrap().len() as u32;
+                        let start = Position { line, character };
+                        let end = Position { line, character };
+                        Range { start, end }
+                    }
+                };
+
+                let diagnostic = Diagnostic {
+                    range,
+                    severity: Some(DiagnosticSeverity::ERROR),
+                    message: e.to_string(),
+                    ..Diagnostic::default()
+                };
+
+                self.client
+                    .publish_diagnostics(doc.uri, vec![diagnostic], None)
+                    .await;
+
                 self.log_info("diagnostic sent").await;
             }
         }
