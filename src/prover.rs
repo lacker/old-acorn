36bcf0f1719fdee8d95b901a096da39e16ffecad
Kevin Lacker <lacker@gmail.com>
2024-04-23 10:07:26 -0700

    allow alt diamond

--- src/prover.rs
+++ src/prover.rs
@@ -1466,7 +1466,17 @@ mod tests {
             f(0)
         }
         "#;
-        expect_proof(text, "f(0)", &[]);
+        expect_proof(
+            text,
+            "f(0)",
+            &[
+                "if !add_to_zero(0, b) {",
+                "\tadd(0, b) = 0",
+                "\tb != 0",
+                "\tfalse",
+                "}",
+            ],
+        );
     }
 
     #[test]
