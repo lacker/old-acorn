fdbacbced8603a45e0031db7c5f90c7bc515f7ed
Kevin Lacker <lacker@gmail.com>
2024-08-29 09:29:35 -0700

    try_monomorphize

--- src/monomorphizer.rs
+++ src/monomorphizer.rs
@@ -58,10 +58,9 @@ pub struct Monomorphizer {
     // Indexed by constant id
     monomorphs_for_constant: HashMap<ConstantKey, Vec<ParamList>>,
 
-    // Where each parametric constant is mentioned in a parametric way.
+    // An index tracking how each polymorphic constant is used in the polymorphic facts.
     // Lists (index in polymorphic facts, params for the constant) for each occurrence.
-    // parametric_instances only needs to be computed once.
-    parametric_instances: HashMap<ConstantKey, Vec<(usize, ParamList)>>,
+    polymorphic_instances: HashMap<ConstantKey, Vec<(usize, ParamList)>>,
 }
 
 impl Monomorphizer {
@@ -73,7 +72,7 @@ impl Monomorphizer {
             monomorphic_facts: vec![],
             monomorphs_for_fact: vec![],
             monomorphs_for_constant: HashMap::new(),
-            parametric_instances: HashMap::new(),
+            polymorphic_instances: HashMap::new(),
         };
         for fact in input_facts {
             m.add_fact(fact);
@@ -106,7 +105,7 @@ impl Monomorphizer {
         self.monomorphs_for_fact.push(vec![]);
         for (constant_key, params) in instances {
             let params = ParamList::new(params);
-            self.parametric_instances
+            self.polymorphic_instances
                 .entry(constant_key)
                 .or_insert(vec![])
                 .push((i, params));
@@ -128,8 +127,10 @@ impl Monomorphizer {
         graph.monomorphic_facts
     }
 
-    // Monomorphizes a polymorphic constant, everywhere it appears.
-    fn monomorphize_constant(&mut self, constant_key: ConstantKey, monomorph_params: &ParamList) {
+    // Monomorphizes a polymorphic constant.
+    // For every fact that uses this constant, we want to monomorphize the fact to use this
+    // form of the polymorphic constant.
+    fn monomorphize_constant(&mut self, constant_key: &ConstantKey, monomorph_params: &ParamList) {
         monomorph_params.assert_monomorph();
         let monomorphs = self
             .monomorphs_for_constant
@@ -142,49 +143,58 @@ impl Monomorphizer {
         monomorphs.push(monomorph_params.clone());
 
         // Handle all the facts that mention this constant without monomorphizing it.
-        if let Some(instances) = self.parametric_instances.get(&constant_key) {
+        if let Some(instances) = self.polymorphic_instances.get(&constant_key) {
             for (fact_id, instance_params) in instances.clone() {
-                // The instance params are the way this instance was parametrized in the fact.
-                // The monomorph params are how we would like to parametrize this instance.
-                // It may or may not be possible to match them up.
-                // For example, this may be a fact about foo<bool, T>, and our goal
-                // is saying something about foo<Nat, Nat>.
-                // Our goal is to find the "fact params", a way in which we can parametrize
-                // the whole fact so that the instance params become the monomorph params.
-                assert_eq!(instance_params.params.len(), monomorph_params.params.len());
-                let mut fact_params = HashMap::new();
-                for ((i_name, instance_type), (m_name, monomorph_type)) in instance_params
-                    .params
-                    .iter()
-                    .zip(monomorph_params.params.iter())
-                {
-                    assert_eq!(i_name, m_name);
-                    instance_type.match_specialized(monomorph_type, &mut fact_params);
-                }
-
-                // We sort because there's no inherently canonical order.
-                let mut fact_params: Vec<_> = fact_params.into_iter().collect();
-                fact_params.sort();
-                let fact_params = ParamList::new(fact_params);
-
-                if self.monomorphs_for_fact[fact_id].contains(&fact_params) {
-                    // We already have this monomorph
-                    continue;
-                }
-
-                let monomorphic_fact =
-                    self.polymorphic_facts[fact_id].specialize(&fact_params.params);
-                if monomorphic_fact.value.is_parametric() {
-                    // This is a little awkward. Completely monomorphizing this instance
-                    // still doesn't monomorphize the whole fact.
-                    // TODO: instead of bailing out, partially monomorphize, and continue.
-                    continue;
-                }
-                self.monomorphs_for_fact[fact_id].push(fact_params);
-                self.match_value(&monomorphic_fact.value);
-                self.monomorphic_facts.push(monomorphic_fact);
+                self.try_monomorphize(fact_id, monomorph_params, &instance_params);
             }
         }
+    }
+
+    // Try to monomorphize the given fact so that the given params match.
+    // The instance params are the way this instance was parametrized in the fact.
+    // The monomorph params are how we would like to parametrize this instance.
+    // It may or may not be possible to match them up.
+    // For example, this may be a fact about foo<bool, T>, and our goal
+    // is saying something about foo<Nat, Nat>.
+    fn try_monomorphize(
+        &mut self,
+        fact_id: usize,
+        monomorph_params: &ParamList,
+        instance_params: &ParamList,
+    ) {
+        // Our goal is to find the "fact params", a way in which we can parametrize
+        // the whole fact so that the instance params become the monomorph params.
+        assert_eq!(instance_params.params.len(), monomorph_params.params.len());
+        let mut fact_params = HashMap::new();
+        for ((i_name, instance_type), (m_name, monomorph_type)) in instance_params
+            .params
+            .iter()
+            .zip(monomorph_params.params.iter())
+        {
+            assert_eq!(i_name, m_name);
+            instance_type.match_specialized(monomorph_type, &mut fact_params);
+        }
+
+        // We sort because there's no inherently canonical order.
+        let mut fact_params: Vec<_> = fact_params.into_iter().collect();
+        fact_params.sort();
+        let fact_params = ParamList::new(fact_params);
+
+        if self.monomorphs_for_fact[fact_id].contains(&fact_params) {
+            // We already have this monomorph
+            return;
+        }
+
+        let monomorphic_fact = self.polymorphic_facts[fact_id].specialize(&fact_params.params);
+        if monomorphic_fact.value.is_parametric() {
+            // This is a little awkward. Completely monomorphizing this instance
+            // still doesn't monomorphize the whole fact.
+            // TODO: instead of bailing out, partially monomorphize, and continue.
+            return;
+        }
+        self.monomorphs_for_fact[fact_id].push(fact_params);
+        self.match_value(&monomorphic_fact.value);
+        self.monomorphic_facts.push(monomorphic_fact);
     }
 
     // Make sure that we are generating any monomorphizations that are used in this value.
@@ -192,7 +202,7 @@ impl Monomorphizer {
         let mut monomorphs = vec![];
         value.find_monomorphs(&mut monomorphs);
         for (constant_key, params) in monomorphs {
-            self.monomorphize_constant(constant_key, &ParamList::new(params));
+            self.monomorphize_constant(&constant_key, &ParamList::new(params));
         }
     }
 }
