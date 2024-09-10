7aa3119818d2054967fad6006d174729bdfa00ac
Kevin Lacker <lacker@gmail.com>
2024-09-10 12:00:10 -0700

    to_array

--- src/features.rs
+++ src/features.rs
@@ -1,3 +1,5 @@
+use ndarray::Array1;
+
 use crate::clause::Clause;
 use crate::proof_step::{Rule, Truthiness};
 
@@ -43,5 +45,19 @@ impl Features {
             proof_size: proof_size as i32,
             depth: depth as i32,
         }
+    }
+
+    pub fn to_array(&self) -> Array1<f32> {
+        Array1::from(vec![
+            self.is_contradiction as i8 as f32,
+            self.atom_count as f32,
+            self.is_counterfactual as i8 as f32,
+            self.is_hypothetical as i8 as f32,
+            self.is_factual as i8 as f32,
+            self.is_assumption as i8 as f32,
+            self.is_negated_goal as i8 as f32,
+            self.proof_size as f32,
+            self.depth as f32,
+        ])
     }
 }
