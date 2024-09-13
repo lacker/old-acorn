236ba4678b9da6c643faea70c5b36dc92597bf23
Kevin Lacker <lacker@gmail.com>
2024-09-13 16:16:36 -0700

    Model.load

--- src/model.rs
+++ src/model.rs
@@ -1,20 +1,34 @@
+use std::path::PathBuf;
+
+use ort::{Error, GraphOptimizationLevel, Session};
+
 use crate::features::Features;
 use crate::scorer::Scorer;
 
 // The Model loads a model that was trained in Python and uses it to score feature vectors.
-pub struct Model;
+pub struct Model {
+    // The ONNX .
+    session: Session,
+}
 
 // We just support one hard-coded model.
 const FILENAME: &str = "model-2024-09-13-09:55:03.onnx";
 
 impl Model {
-    pub fn new() -> Self {
-        todo!();
+    pub fn load() -> Result<Self, Error> {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("files");
+        d.push(FILENAME);
+        let session = Session::builder()?
+            .with_optimization_level(GraphOptimizationLevel::Level3)?
+            .commit_from_file(d)?;
+
+        Ok(Model { session })
     }
 }
 
 impl Scorer for Model {
-    fn score(&self, features: &Features) -> f32 {
+    fn score(&self, _features: &Features) -> f32 {
         todo!();
     }
 }
@@ -27,7 +41,7 @@ mod tests {
 
     #[test]
     fn test_onnx_scoring() {
-        let model = Model::new();
+        let model = Model::load().unwrap();
         let step = ProofStep::mock("c0(c3) = c2");
         let features = Features::new(&step);
         let score = model.score(&features);
