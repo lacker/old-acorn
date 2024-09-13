8a528736fe162f7a79f5794a6ec5e19fc878cebd
Kevin Lacker <lacker@gmail.com>
2024-09-13 15:41:41 -0700

    hit the todo

--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,10 @@
 use crate::features::Features;
 use crate::scorer::Scorer;
 
+// The Model loads a model that was trained in Python and uses it to score feature vectors.
 pub struct Model;
 
+// We just support one hard-coded model.
 const FILENAME: &str = "model-2024-09-13-09:55:03.onnx";
 
 impl Model {
@@ -14,5 +16,21 @@ impl Model {
 impl Scorer for Model {
     fn score(&self, features: &Features) -> f32 {
         todo!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::proof_step::ProofStep;
+
+    use super::*;
+
+    #[test]
+    fn test_onnx_scoring() {
+        let model = Model::new();
+        let step = ProofStep::mock("c0(c3) = c2");
+        let features = Features::new(&step);
+        let score = model.score(&features);
+        assert!(score.is_finite());
     }
 }
