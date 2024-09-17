639658aa2aaa4eac029b84f7e50a7ac4e54f7f55
Kevin Lacker <lacker@gmail.com>
2024-09-17 11:02:57 -0700

    print scores per second

--- src/bin/profile_scorer.rs
+++ src/bin/profile_scorer.rs
@@ -10,16 +10,22 @@ use acorn::proof_step::ProofStep;
 use acorn::scorer::Scorer;
 
 fn main() {
-    for i in 1..11 {
+    let m = 10;
+    let n = 100000;
+    let mut total_seconds = 0.0;
+    for i in 1..(m + 1) {
         let scorer = ScoringModel::load(true).unwrap();
         let step = ProofStep::mock("c0(c3) = c2");
         let features = Features::new(&step);
         let start = std::time::Instant::now();
-        for _ in 0..100000 {
+        for _ in 0..n {
             let score = scorer.score(&features).unwrap();
             assert!(score.is_finite());
         }
-        let elapsed = start.elapsed();
-        println!("batch {} took {:.3} seconds", i, elapsed.as_secs_f64());
+        let elapsed = start.elapsed().as_secs_f32();
+        total_seconds += elapsed;
+        println!("batch {} took {:.3} seconds", i, elapsed);
     }
+    let nps = (n * m) as f32 / total_seconds;
+    println!("{:.1} scores per second", nps);
 }
